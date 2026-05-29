079b05c9b58fb7f79a5e38a393e75be417eb02cf
Chekito2720 <2023371075@uteq.edu.mx>
2026-05-29 17:50:13 -0600

    Inicio de proyecto

---
+++ tacomoloco/src/main/java/tacomoloco/tacomoloco/TacomolocoApplication.java
@@ -0,0 +1,13 @@
+package tacomoloco.tacomoloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacomolocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacomolocoApplication.class, args);
+	}
+
+}
---
+++ tacomoloco/src/test/java/tacomoloco/tacomoloco/TacomolocoApplicationTests.java
@@ -0,0 +1,13 @@
+package tacomoloco.tacomoloco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacomolocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
